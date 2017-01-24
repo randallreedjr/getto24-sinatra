b728f069e8876ad12c3e1869e8ae889482baaf3f
Randall Reed, Jr <randallreedjr@gmail.com>
2017-01-24 14:35:25 -0500

    Update app to use new syntax for Math24 gem
    *Delete unused controller action 'post /problem'
    *Delete lots of comments
    *Add tests for controller
    *Remove dependency on Math24 last_answer instance variable
    *Fix some whitespace errors
    *Delete capture_stdout method from spec_helper
    *Other minor refactoring

--- app/controllers/math24_controller.rb
+++ app/controllers/math24_controller.rb
@@ -7,15 +7,8 @@ class Math24Controller < ApplicationController
     erb :'math24/about'
   end
 
-  get '/problem' do 
-    math24 = Math24.new
-    problem = math24.generate_problem.join(" ")
-    erb :'math24/problem', :locals => {:problem => problem}
-  end
-
-  post '/problem' do
-    math24 = Math24.new
-    problem = "#{params[:problem] || ""}"
+  get '/problem' do
+    problem = Math24.generate_problem.join(" ")
     erb :'math24/problem', :locals => {:problem => problem}
   end
 
@@ -24,20 +17,22 @@ class Math24Controller < ApplicationController
   end
 
   post '/solution' do
-    math24solver = Math24Solver.new
     problem = "#{params[:problem] || ""}"
     if problem.include?(",")
-      numbers = problem.gsub(" ","").split(",")
+      numbers = problem.delete(" ").split(",")
     else
-      numbers = problem.squeeze(" ").split(" ")
+      numbers = problem.squeeze(" ").split
     end
+
     valid = numbers.all? do |number|
       number.to_i > 0 && number.to_i < 10
     end
+
     if valid
-      solution = math24solver.solve(numbers)
-      erb :'math24/solution', :locals => {:problem => numbers.join(" "), 
-                                 :solution => solution,
+      solution = Math24.solve(numbers)
+      message = solution ? "#{solution} = 24" : "No solution found"
+      erb :'math24/solution', :locals => {:problem => numbers.join(" "),
+                                 :message => message,
                                  :last_answer => 24}
     else
       erb :'math24/solve', :locals => {:invalid => true}
@@ -47,16 +42,29 @@ class Math24Controller < ApplicationController
   post '/check' do
     problem = "#{params[:problem] || ""}"
     solution = "#{params[:solution] || ""}"
-    math24 = Math24.new
-    math24.numbers = problem.split(" ")
-    if math24.solution?(solution)
-      erb :'math24/correct', :locals => {:problem => problem, 
-                                :solution => solution, 
-                                :last_answer => math24.last_answer}
+    numbers = problem.split
+
+    begin
+      valid_solution = Math24.check(numbers, solution)
+    rescue ArgumentError
+      error = true
+      valid_solution = false
+    end
+
+    if valid_solution
+      erb :'math24/correct',
+          :locals => {
+            :problem => problem,
+            :solution => solution
+          }
+    elsif error
+      erb :'math24/incorrect', :locals => {:problem => problem,
+                                  :solution => solution,
+                                  :last_answer => '???'}
     else
-      erb :'math24/incorrect', :locals => {:problem => problem, 
-                                  :solution => solution, 
-                                  :last_answer => math24.last_answer}
+      erb :'math24/incorrect', :locals => {:problem => problem,
+                                  :solution => solution,
+                                  :last_answer => instance_eval(solution)}
     end
   end
 end
